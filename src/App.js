6605903b1066f2503f2ed0958cd96c3cc9fab6a1
Tosin <tosin@sourcebreaker.com>
2020-06-11 02:11:46 +0100

    WIP signIn page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './components/pages/sign-inand-sign-up/sign-inand-sign-up.component';
 function App() {
 	return (
 		<div>
@@ -12,6 +13,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route exact path="/shop" component={ShopPage} />
+				<Route exact path="/signin" component={SignInAndSignUpPage} />
 			</Switch>
 		</div>
 	);
