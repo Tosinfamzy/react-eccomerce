f4de1816e55ba4621a68871fd3743ad2b9eb414e
Tosin <tosin@sourcebreaker.com>
2020-06-11 01:23:26 +0100

    Header component  with links

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
+import Header from './components/header/header.component';
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route exact path="/shop" component={ShopPage} />
